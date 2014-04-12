4b2d1a866ada6d4c2b870079499414029a877c7e
easonlin <smartsgy@gmail.com>
2014-04-13 01:40:33 +0800

    [add] web debug

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 from flask import Flask
 app = Flask(__name__)
+app.config["DEBUG"] = True
+# wrap the application
+from werkzeug import DebuggedApplication
+app.wsgi_app = DebuggedApplication(app.wsgi_app, evalex=True)
 import json
 # Note: We don't need to call run() since our application is embedded within
 # the App Engine WSGI application server.
 
 
 @app.route('/test')
-def hello():
+def test():
     """Return a friendly HTTP greeting."""
     return json.dumps({"Foo": ["bar", "can", "haz"]})
+
+@app.route('/post')
+def post():
+    """ creat post """
+    raise Exception("Hello")
+
 
 @app.errorhandler(404)
 def page_not_found(e):
